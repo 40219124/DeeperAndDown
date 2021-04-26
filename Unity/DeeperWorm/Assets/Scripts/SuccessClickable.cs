c81818cac80091906f50e316323cfd9c3ce5185d
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2021-04-27 00:51:05 +0100

    another popup

--- Unity/DeeperWorm/Assets/Scripts/SuccessClickable.cs
+++ Unity/DeeperWorm/Assets/Scripts/SuccessClickable.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SuccessClickable : MonoBehaviour, IOurClickable
 {
+    [SerializeField] bool RestartGame;
     public void Clicked()
     {
         if (GameAssistant.Running)
         {
             GetComponentInParent<MinigameController>().MiniGameSuccess(true);
+
+            if (RestartGame)
+            {
+                SceneManager.LoadScene("Opening");
+            }
         }
     }
 }
